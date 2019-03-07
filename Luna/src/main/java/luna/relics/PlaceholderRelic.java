2ff6f9b9a95f84c64e3a5ed46ca746794c57171f
SacredDiamond <45544974+SacredDiamond@users.noreply.github.com>
2019-03-07 08:08:17 -0500

    Initial commit

---
+++ Luna/src/main/java/luna/relics/PlaceholderRelic.java
@@ -0,0 +1,55 @@
+package luna.relics;
+
+import basemod.abstracts.CustomRelic;
+import luna.LunaBase;
+import luna.util.TextureLoader;
+
+import static luna.LunaBase.makeRelicOutlinePath;
+import static luna.LunaBase.makeRelicPath;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class PlaceholderRelic extends CustomRelic {
+
+    /*
+     * https://github.com/daviscook477/BaseMod/wiki/Custom-Relics
+     *
+     * Gain 1 energy.
+     */
+
+    // ID, images, text.
+    public static final String ID = LunaBase.makeID("PlaceholderRelic");
+
+    private static final Texture IMG = TextureLoader.getTexture(makeRelicPath("placeholder_relic.png"));
+    private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath("placeholder_relic.png"));
+
+    public PlaceholderRelic() {
+        super(ID, IMG, OUTLINE, RelicTier.STARTER, LandingSound.MAGICAL);
+    }
+
+    // Flash at the start of Battle.
+    @Override
+    public void atBattleStartPreDraw() {
+        flash();
+    }
+
+    // Gain 1 energy on equip.
+    @Override
+    public void onEquip() {
+        AbstractDungeon.player.energy.energyMaster += 1;
+    }
+
+    // Lose 1 energy on unequip.
+    @Override
+    public void onUnequip() {
+        AbstractDungeon.player.energy.energyMaster -= 1;
+    }
+
+    // Description
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0];
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# StS-the_shining_moon
